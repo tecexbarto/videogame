bbbb0f1bf41c78bf6a9e1cbea74d58876d8b53ed
tecexbarto <137276358+tecexbarto@users.noreply.github.com>
2024-03-31 22:09:33 +0200

    Haciendo una modificación del archivo videojuego

--- videojuego.py
+++ videojuego.py
@@ -253,3 +253,5 @@ class Muro(pygame.sprite.Group):
 director = Director('Juego de ladrillos', (ANCHO, ALTO))
 director.agregarEscena('Nivel1')
 director.ejecutar('Nivel1')
+
+"Voy a realizar un cambio en el codigo"
